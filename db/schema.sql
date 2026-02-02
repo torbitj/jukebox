fff75962040821ef4c27d06babd7bf7f39e9ea32
JT <jstorbit@gmail.com>
2026-02-02 15:09:43 -0700

    Add base schema dropping tables and creating without table keys

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
--- TODO
+DROP IF EXISTS playlist_tracks;
+DROP IF EXISTS tracks;
+DROP IF EXISTS playlist;
+
+CREATE TABLE playlist (
+
+);
+
+CREATE TABLE tracks (
+
+);
+
+CREATE TABLE playlist_tracks (
+
+);

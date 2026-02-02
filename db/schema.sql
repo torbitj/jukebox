be70f1c14c3e3da2066c781d23032d5dc0f27ff2
JT <jstorbit@gmail.com>
2026-02-02 15:23:22 -0700

    Add table keys and constraints

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,22 @@
-DROP IF EXISTS playlist_tracks;
-DROP IF EXISTS tracks;
-DROP IF EXISTS playlist;
+DROP TABLE IF EXISTS playlist_tracks;
+DROP TABLE IF EXISTS tracks;
+DROP TABLE IF EXISTS playlists;
 
-CREATE TABLE playlist (
-
+CREATE TABLE playlists (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  description TEXT NOT NULL
 );
 
 CREATE TABLE tracks (
-
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  duration INTEGER NOT NULL
 );
 
 CREATE TABLE playlist_tracks (
-
+  id SERIAL PRIMARY KEY,
+  playlist_id INTEGER NOT NULL REFERENCES playlists.id ON DELETE CASCADE,
+  track_id INTEGER NOT NULL REFERENCES tracks.id ON DELETE CASCADE,
+  UNIQUE (playlist_id, track_id)
 );

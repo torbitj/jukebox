1939028a0b6fa54bd950532346f01fdbb0145979
JT <jstorbit@gmail.com>
2026-02-02 17:02:30 -0700

    Add get route for all playlists

--- api/playlists.js
+++ api/playlists.js
@@ -1,4 +1,10 @@
+import { getAllPlaylists } from '#db/queries/playlists';
 import express from 'express';
 
 const playlistsRouter = express.Router();
 export default playlistsRouter;
+
+playlistsRouter.get('/', async(req, res, next) => {
+  const playlists = await getAllPlaylists();
+  res.send(playlists);
+})
--- db/queries/playlists.js
+++ db/queries/playlists.js
@@ -10,3 +10,12 @@ export const createPlaylist = async ({ name, description }) => {
   const { rows: [playlist] } = await db.query(sql, [name, description]);
   return playlist;
 }
+
+export const getAllPlaylists = async () => {
+  const sql = `
+    SELECT * FROM playlists
+  `;
+
+  const { rows: playlists } = await db.query(sql);
+  return playlists;
+}
